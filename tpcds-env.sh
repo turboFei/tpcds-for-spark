98a4dbf4ca7692aa9ac9ef99f79e9874f7aac29c
Fei Wang <fwang12@ebay.com>
2025-05-15 03:28:49 -0700

    save

--- tpcds-env.sh
+++ tpcds-env.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-bin="$(cd "$(dirname "$0")";pwd)"
+bin="$(cd "$(dirname "$0")"; pwd)"
 
 # the path to the tpcds-kit directory
 export TPCDS_ROOT=$bin/tpcds-kit
 
-# the table list file of tpcds
+# the table list file for tpcds
 export tableNameList=$bin/tableList
 
-# HDFS上存放文件的根目录地址
+# HDFS root directory for storing files
 export FLATFILE_HDFS_ROOT=/user/hive/tpcds_data
 #export FLATFILE_HDFS_ROOT=/user/hdfs/tpcds_data_test
 #export FLATFILE_HDFS_ROOT=/user/hive/tpcds_data_1G
 
-# 本地存放文件的根目录地址
+# local directory root for storing files
 export LOCAL_DATA_DIR=$bin/Data
 
-# 原始数据大小(scale factor in GB)
+# original data size (scale factor in GB)
 # SF 3000 yields ~1TB for the store_sales table
 export TPCDS_SCALE_FACTOR=1000
 
-# this is used to determine the number of dsdgen processes to generate data
+# determine the number of dsdgen processes for data generation
 # usually set to one per physical CPU core
 # example - 20 nodes @ 12 threads each
 export DSDGEN_NODES=3
 export DSDGEN_THREADS_PER_NODE=30
 export DSDGEN_TOTAL_THREADS=$((DSDGEN_NODES * DSDGEN_THREADS_PER_NODE))
 
-# Database名称
+# database name
 export TPCDS_DBNAME=tpcds
 
 export SPARK_HOME=/home/hadoop/spark-2.1.0-bin-hadoop2.7
